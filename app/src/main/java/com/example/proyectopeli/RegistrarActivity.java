70860346c59a89a0375e573e0292d58701754902
Paulo Ramos <anuelitoramos@gmail.com>
2023-11-17 21:07:16 -0500

    git add.

--- app/src/main/java/com/example/proyectopeli/RegistrarActivity.java
+++ app/src/main/java/com/example/proyectopeli/RegistrarActivity.java
@@ -2,13 +2,28 @@ package com.example.proyectopeli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegistrarActivity extends AppCompatActivity {
 
+    TextView txtregresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrar);
+
+        txtregresar = (TextView) findViewById(R.id.txtRegresar);
+
+        txtregresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegistrarActivity.this , LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
