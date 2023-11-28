efed62700afa13cc2f651045b609576eb59810a9
Paulo Ramos <anuelitoramos@gmail.com>
2023-11-27 23:25:47 -0500

    git push

--- app/src/main/java/com/example/proyectopeli/MenuPeli.java
+++ app/src/main/java/com/example/proyectopeli/MenuPeli.java
@@ -2,15 +2,13 @@ package com.example.proyectopeli;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import com.example.proyectopeli.databinding.ActivityMenuPeliBinding;
+import  com.example.proyectopeli.databinding.ActivityMenuPeliBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuPeli extends AppCompatActivity {
 
@@ -19,9 +17,6 @@ public class MenuPeli extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-       // Asegúrate de reemplazar "R.id.toolbar" con el ID de tu Toolbar
-
 
         binding = ActivityMenuPeliBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,7 +27,7 @@ public class MenuPeli extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
